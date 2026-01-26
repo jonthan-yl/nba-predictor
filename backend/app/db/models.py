ea846db43dcee5f5f94de2d51838e332319b51d4
jonthan-yl <89768805+jonthan-yl@users.noreply.github.com>
2026-01-26 12:25:02 -0600

    updating to model shape and created and ran scripts to generate initial data, more information in README

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -1,13 +1,34 @@
-from sqlalchemy import Column, Integer, String, Float, Date
+from sqlalchemy import Column, Integer, String, Float, Date, Boolean, UniqueConstraint
 from .database import Base
 
 class PlayerGameLog(Base):
     __tablename__ = "player_game_logs"
 
     id = Column(Integer, primary_key=True, index=True)
-    player_id = Column(Integer, index=True)
+    player_id = Column(String, index=True)
     game_date = Column(Date, index=True)
     minutes = Column(Float)
     points = Column(Integer)
     rebounds = Column(Integer)
     assists = Column(Integer)
+    steals = Column(Integer)
+    blocks = Column(Integer)
+    turnovers = Column(Integer)
+    field_goals_made = Column(Integer)
+    field_goals_attempted = Column(Integer)
+    three_points_made = Column(Integer)
+    three_points_attempted = Column(Integer)
+    free_throws_made = Column(Integer)
+    free_throws_attempted = Column(Integer)
+    personal_fouls = Column(Integer)
+
+class Player(Base):
+    __tablename__ = "players"
+
+    id = Column(String, primary_key=True, index=True)
+    full_name = Column(String, index=True)
+    height = Column(Integer, nullable=True)
+    weight = Column(Integer, nullable=True)
+    position = Column(String)
+    is_active = Column(Boolean, default=True)
+    birthdate = Column(Date, nullable=True)
